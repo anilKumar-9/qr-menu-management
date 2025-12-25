552c6414f9c620e7eee24e8d5c019eb08052ce18
Anil Kumar <nagapuri.anil.tech@gmail.com>
2025-12-25 17:58:43 +0530

    reslove show menu

--- frontend/src/api/menu.api.js
+++ frontend/src/api/menu.api.js
@@ -3,4 +3,5 @@ import instance from "../../utils/axios";
 export const createMenu = (restaurantId, data) =>
   instance.post(`/menus/${restaurantId}`, data);
 
-export const getMenus = (restaurantId) => instance.get(`/menus/${restaurantId}`);
+export const getMenus = (restaurantId) =>
+  instance.get(`/menus/restaurants/${restaurantId}`);
